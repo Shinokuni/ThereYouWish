c2c1ea453c64e2a9c04be3bb6ff4cba489c66ea3
Shinokuni <shinokuni@openserver.fr>
2025-05-06 15:25:51 +0200

    Add theme

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import React, {Suspense} from 'react';
-import {AppRegistry} from 'react-native';
-import {PaperProvider} from 'react-native-paper';
+import {AppRegistry, useColorScheme} from 'react-native';
+import {MD3DarkTheme, PaperProvider} from 'react-native-paper';
 
 import App from './App';
 import {name as appName} from './app.json';
 import Icon from '@react-native-vector-icons/material-design-icons';
+import {MD3LightTheme} from 'react-native-paper';
+import LightTheme from './util/theme/light_theme.json';
+import DarkTheme from './util/theme/dark_theme.json';
+
+const lightTheme = {
+  ...MD3LightTheme,
+  colors: LightTheme.colors,
+};
+
+const darkTheme = {
+  ...MD3DarkTheme,
+  colors: DarkTheme.colors,
+};
 
 const TYWIcon = props => {
   return <Icon {...props} />;
@@ -13,6 +26,7 @@ const TYWIcon = props => {
 export default function Main() {
   return (
     <PaperProvider
+      theme={useColorScheme() === 'light' ? lightTheme : darkTheme}
       settings={{
         icon: props => TYWIcon(props),
       }}>
